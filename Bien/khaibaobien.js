bc55ee9fe55767eaa6572fe8a63033ad79398c0a
Admin <hanhngao@gmail.com>
2022-06-08 22:23:43 +0700

    sua tt doi tien

---
+++ Bien/khaibaobien.js
@@ -0,0 +1,29 @@
+// document.write("---------------<br/>")
+//
+// let width = 20
+// let height = 10
+// let area = width * height
+// document.write('Area = ' + area)
+// document.write("<br/>---------------<br/>")
+// let i = 10
+// let f = 20.5
+// let b = true
+// let s = 'Hà Nội'
+//
+// document.write('i = ' + i)
+// document.write('<br/>')
+// document.write('f = ' + f)
+// document.write('<br/>')
+// document.write('b = ' + b)
+// document.write('<br/>')
+// document.write("<br/>---------------<br/>")
+let a = +prompt("nhập a")
+let b = +prompt("nhập b")
+if (a % b == 0){
+    alert("a chi hết cho b")
+}else {
+    alert("không chia hêt")
+}
+
+
+
