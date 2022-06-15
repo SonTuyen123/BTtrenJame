dfb02c22e6ce99ddccffd659616fc40348e27a7e
Admin <hanhngao@gmail.com>
2022-06-15 11:01:03 +0700

    add

---
+++ [TH]/Co_Ca_Ro/Co.js
@@ -0,0 +1,24 @@
+let b = document.getElementById('caro');
+let array = [];
+let data = "";
+for (let i = 0; i < 5; i++) {
+    array[i] = new Array("o","o","o","o","o");
+    data += '<br>'
+    for (let j = 0; j < 5; j++) {
+        data += array[i][j];
+    }
+}
+document.write(data)
+function changeValue(){
+    let positionX = prompt('X :');
+    let positionY = prompt('Y :');
+    data = "";
+    array[positionX][positionY] = 'x';
+    for(let i = 0; i < 5;i++){
+        data += '<br>';
+        for(let j = 0;j < 5;j++){
+            data += array[i][j]
+        }
+    }
+    data += b.innerHTML = data;
+}
