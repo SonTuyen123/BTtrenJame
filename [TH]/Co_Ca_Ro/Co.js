4b0c3d477db615a3c1aee78639f0e67295650b64
Admin <hanhngao@gmail.com>
2022-06-16 10:02:53 +0700

    add

--- [TH]/Co_Ca_Ro/Co.js
+++ [TH]/Co_Ca_Ro/Co.js
@@ -7,7 +7,7 @@ for (let i = 0; i < 5; i++) {
     for (let j = 0; j < 5; j++) {
         data += array[i][j];
     }
-}
+}console.
 document.write(data)
 function changeValue(){
     let positionX = prompt('X :');
